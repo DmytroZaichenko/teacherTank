d3761b4ee6068715ca2dce499d14db39394aceab
Dmytro Zaichenko <dim2002@ukr.net>
2015-11-02 11:34:06 +0200

    ver3

---
+++ src/ActionField.java
@@ -0,0 +1,44 @@
+/**
+ * Created by DmitryZ on 02.11.2015.
+ */
+public class ActionField {
+
+    public final boolean COLORED_MODE = false;
+    BattleField battleField;
+    Tank tank;
+    Bullet bullet;
+
+    public ActionField(){
+
+    }
+
+    public void runTheGame(){
+
+    }
+
+    public boolean processInterception(){
+        return false;
+    }
+
+    public void getQuadrant(int v, int h){
+
+    }
+
+    public void getQuadrantXY(int v, int h){
+
+    }
+
+    public void processTurn(Tank tank){
+
+    }
+
+    public void processMove(Tank tank){
+
+    }
+
+    public void processFire(Bullet bullet){
+
+    }
+
+
+}
---
+++ src/Tank.java
@@ -0,0 +1,5 @@
+/**
+ * Created by DmitryZ on 02.11.2015.
+ */
+public class Tank {
+}
